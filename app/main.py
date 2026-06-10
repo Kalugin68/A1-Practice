e1b2963ddf4f49ff1b18a29b725cc9426b443104
Kalugin68 <sashakalugin74@gmail.com>
2026-06-10 11:15:39 +0300

    Initial commit

---
+++ app/main.py
@@ -0,0 +1,26 @@
+import smtplib
+from fastapi import FastAPI
+from email.message import EmailMessage
+from schemas import EmailRequest
+
+app = FastAPI()
+
+@app.post("/send")
+def send_email(data: EmailRequest):
+    msg = EmailMessage()
+
+    msg["Subject"] = "Test message"
+    msg["From"] = "sashakalugin74@gmail.com"
+    msg["To"] = data.email
+
+    msg.set_content("Hello World")
+
+    with smtplib.SMTP("mailpit", 1025) as smtp:
+        smtp.send_message(msg)
+
+    return {"message": "Email sent successfully",
+            "email": data.email}
+
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
